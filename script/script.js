de4ae5654cd50048f2c0bc85ec2f129683d247b9
FedericaDiChio <federica.dichio7@gmail.com>
2021-07-11 13:32:27 +0200

    variabili, elemento html

--- script/script.js
+++ script/script.js
@@ -7,37 +7,10 @@
 6. bisogna stampare in pagina;
 */
 
-// Variabili
+// HTML Elements 
 var listElement = document.getElementById("lastname-list");
 var displayPosition= document.getElementById("position")
+
+// Variables
 var lastname = ["Bianchi", "Neri", "Rossi", "Verdi", "Gialli"];
-
-// Dati utente
-var userLastname;
-
-do {
-    userLastname = prompt("Inserisci il tuo cognome");
-} while (!userLastname);
-
-
-// console.log(lastname);
-// console.log(userLastname);
-
-// Aggiungere e ordinare alfabeticamente
-lastname.push(userLastname);
-lastname.sort();
-
-// Stampare la posizione del cognome utente 
-var userPosition = lastname.indexOf(userLastname) + 1; 
-displayPosition.innerHTML = "Il cognome si trova in posizione" + " " + userPosition;
-
-
-// In pagina
-var listItems = "";
-for (var i = 0; i < lastname.length; i++) {
-    listItems += "<li>" + lastname[i] + "</li>";
-
-}
-
-listElement.innerHTML = listItems;
 
